49b50fac045c02ce033434c89c02472677cea450
minoru.nagasawa <minoru.nagasawa@gmail.com>
2018-07-08 23:25:30 +0900

    Initial Commit

---
+++ PerformanceInterpolationMode/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace PerformanceInterpolationMode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new PerformanceInterpolationMode.PictureBox2();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(776, 426);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PictureBox2 pictureBox2;
+    }
+}
+
---
+++ PerformanceInterpolationMode/PictureBox2.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerformanceInterpolationMode
+{
+    class PictureBox2 : PictureBox
+    {
+        private Image m_image;
+        private InterpolationMode[] m_interpolations;
+        private Dictionary<InterpolationMode, long> m_totalTicks;
+        private int m_count;
+        private Timer m_timer;
+        public PictureBox2()
+        {
+            m_image = new Bitmap(640, 480);
+            m_interpolations = new InterpolationMode[]
+            {
+                InterpolationMode.Default,
+                InterpolationMode.Low,
+                InterpolationMode.High,
+                InterpolationMode.Bilinear,
+                InterpolationMode.Bicubic,
+                InterpolationMode.NearestNeighbor,
+                InterpolationMode.HighQualityBilinear,
+                InterpolationMode.HighQualityBicubic
+            };
+            m_totalTicks = new Dictionary<InterpolationMode, long>();
+            foreach (var v in m_interpolations)
+            {
+                m_totalTicks[v] = 0;
+            }
+            m_timer = new Timer();
+            m_timer.Interval = 500;
+            m_timer.Tick += (s, e) =>
+            {
+                this.Refresh();
+            };
+            m_timer.Start();
+
+            this.Disposed += (s, e) =>
+            {
+                using (var writer = new StreamWriter(@"D:\performance.txt"))
+                {
+                    writer.WriteLine($"{m_count}");
+                    foreach (var v in m_totalTicks)
+                    {
+                        writer.WriteLine($"{v.Key}\t{v.Value}");
+                    }
+                }
+            };
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            var shuffled = m_interpolations.OrderBy(i => Guid.NewGuid());
+            foreach (var v in shuffled)
+            {
+                var sw = new Stopwatch();
+                sw.Start();
+                e.Graphics.InterpolationMode = v;
+                e.Graphics.DrawImage(m_image, 0, 0, this.Width, this.Height);
+                sw.Stop();
+                m_totalTicks[v] = m_totalTicks[v] + sw.ElapsedTicks;
+            }
+            ++m_count;
+        }
+    }
+}
